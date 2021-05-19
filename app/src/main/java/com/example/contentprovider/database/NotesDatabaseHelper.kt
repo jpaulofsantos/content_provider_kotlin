134a1a4a6c183e548f7003af7c84dedea16a3a8a
João Paulo Faustino Santos <jpaulofsantos@gmail.com>
2021-05-19 14:45:42 -0300

    concluding Content Provider

---
+++ app/src/main/java/com/example/contentprovider/database/NotesDatabaseHelper.kt
@@ -0,0 +1,22 @@
+package com.example.contentprovider.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class NotesDatabaseHelper(context:Context): SQLiteOpenHelper(context, "databaseNotes", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    companion object {
+        //tabelas com primeira letra maiuscula e colunas com a primeira letra minuscula
+        const val TABLE_NOTES: String = "Notes"
+        const val TITLE: String = "title"
+        const val DESCRIPTION_NOTES: String = "description"
+    }
+}
