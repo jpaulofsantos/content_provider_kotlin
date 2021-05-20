3b8056b9c0ca0434a5abcde58553a369a2058273
João Paulo Faustino Santos <jpaulofsantos@gmail.com>
2021-05-20 16:16:08 -0300

    adapter, clicklistener

--- app/src/main/java/com/example/contentprovider/MainActivity.kt
+++ app/src/main/java/com/example/contentprovider/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.contentprovider
 
 import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.BaseColumns._ID
+import android.widget.LinearLayout
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
 import com.example.contentprovider.database.NotesProvider.Companion.URI_NOTES
@@ -15,6 +19,7 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
 
     lateinit var noteRecyclerView: RecyclerView
     lateinit var noteAdd: FloatingActionButton
+    lateinit var adapter: NotesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +29,24 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
         noteAdd.setOnClickListener{
 
         }
+        //primeiro criar o adapter, depois realizar a instancia e set de atributos
+        adapter = NotesAdapter(object : NoteClickedListener {
+            override fun noteClickedItem(cursor: Cursor) {
+                val id:Long = cursor?.getLong(cursor.getColumnIndex(_ID))
+            }
 
+            override fun noteRemoveItem(cursor: Cursor?) {
+                val id: Long? = cursor?.getLong(cursor.getColumnIndex(_ID))
+                contentResolver.delete(Uri.withAppendedPath(URI_NOTES, id.toString()), null, null)
+            }
+
+        })
+        adapter.setHasStableIds(true)
+
+        //iniciando o Recycler view por sua view, aplicar um layout e passar no recycler
         noteRecyclerView = findViewById(R.id.notes_recycler)
+        noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        noteRecyclerView.adapter = adapter
 
     }
 
--- app/src/main/java/com/example/contentprovider/NotesAdapter.kt
+++ app/src/main/java/com/example/contentprovider/NotesAdapter.kt
@@ -1,25 +1,54 @@
 package com.example.contentprovider
 
+import android.database.Cursor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.contentprovider.database.NotesDatabaseHelper.Companion.DESCRIPTION_NOTES
+import com.example.contentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
 
-class NotesAdapter : RecyclerView.Adapter<NotesViweHolder> () {
+class NotesAdapter(private val listener: NoteClickedListener) : RecyclerView.Adapter<NotesViweHolder> () {
+
+    private var cursor: Cursor? = null
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViweHolder =
         NotesViweHolder(LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false))
 
-    override fun onBindViewHolder(holder: NotesViweHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: NotesViweHolder, position: Int) { //pegando as strings das colunas do database (baseado na position do viewholder)
+        cursor?.moveToPosition(position)
+        holder.noteTitle.text = cursor?.getString(cursor?.getColumnIndex(TITLE_NOTES) as Int)
+        holder.noteDescription.text = cursor?.getString(cursor?.getColumnIndex(DESCRIPTION_NOTES) as Int)
+        holder.noteButtomRemove.setOnClickListener {
+            cursor?.moveToPosition(position)
+            listener.noteRemoveItem(cursor)
+            notifyDataSetChanged()
+        }
+
+        holder.itemView.setOnClickListener{
+            listener.noteClickedItem(cursor as Cursor)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = if (cursor != null) cursor?.count as Int else 0
+
+    fun setCursor(newCursor: Cursor?) {
+        cursor = newCursor
+        notifyDataSetChanged()
+
     }
 
 
 }
 
 class NotesViweHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val noteTitle = itemView.findViewById(R.id.note_title) as TextView
+    val noteDescription = itemView.findViewById(R.id.note_description) as TextView
+    val noteButtomRemove = itemView.findViewById(R.id.note_buttom_remove) as Button
+
+
 
 }
